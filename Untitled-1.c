62d676e9ad9d9788e0224377310b471d1b97a3a6
SonUHyeok <165031197+SonUHyeok@users.noreply.github.com>
2024-03-26 21:45:22 +0900

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+typedef int* intptr;
+
+int main ()
+{
+    intptr pa;
+    int a;
+    printf("숫자 입력하십시오: ");
+    scanf(%d, &a);
+    printf("입력하신 숫자는 %d 입니다.\n", a)
+    printf("메모리 탐색 시작위치 : %")
+}
